b4821b9c7785a7d42d63269e108e0d2f8d09ea1b
Joshua <jjoshua.claes@gmail.com>
2019-04-30 14:02:47 +0200

    messagetest

---
+++ main.cpp
@@ -0,0 +1,42 @@
+//Joshua Claes
+//joshua.claes@student.uantwerpen.be
+//datum: 22/04/2019
+//STL
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <iomanip>
+
+//OpenCV
+#include <opencv2/opencv.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+using namespace cv;
+//my headers
+#include "ellipsfinder.h"
+
+
+int main(int argc, char *argv[])
+{
+    Mat image(256,256, CV_8UC1, Scalar(0));
+    image.at<uchar>(20,20)=255;
+    image.at<uchar>(20,50)=255;
+    image.at<uchar>(20,60)=255;
+    image.at<uchar>(50,20)=255;
+
+    image.at<uchar>(25,20)=25;
+
+    Ellipsfinder test(image);
+
+
+
+
+
+    return 0;
+}
+
+
+
